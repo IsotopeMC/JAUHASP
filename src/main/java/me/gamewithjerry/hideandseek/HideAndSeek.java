d6b264e0b300b181e22c93cf0b399f0aed2fe13a
Jeremy <jeremy.scheibner@icloud.com>
2021-05-01 17:58:59 +0200

    kopiere Lobbymap bei Serverstart.

--- src/main/java/me/gamewithjerry/hideandseek/HideAndSeek.java
+++ src/main/java/me/gamewithjerry/hideandseek/HideAndSeek.java
@@ -1,16 +1,21 @@
 package me.gamewithjerry.hideandseek;
 
+import jdk.jfr.internal.Utils;
 import me.gamewithjerry.hideandseek.game.GameState;
 import me.gamewithjerry.hideandseek.game.GameTeam;
 import me.gamewithjerry.hideandseek.game.GameUtils;
 import me.gamewithjerry.hideandseek.items.ListenerCollection;
+import me.gamewithjerry.hideandseek.javautils.UtilsCollection;
 import me.gamewithjerry.hideandseek.listener.GameProtectionListener;
 import me.gamewithjerry.hideandseek.listener.PlayerJoinQuitListener;
 import me.gamewithjerry.hideandseek.listener.PlayerLoginListener;
 import org.bukkit.Bukkit;
 import org.bukkit.Color;
+import org.bukkit.World;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.List;
 
 public final class HideAndSeek extends JavaPlugin {
 
@@ -20,6 +25,9 @@ public final class HideAndSeek extends JavaPlugin {
     public final String PREFIX = "§7[§eHide§2N§6Seek§7]§r ";
     public final String NO_PERMISSIONS = PREFIX + "§4Dazu hast du keine Rechte!";
 
+    public List<World> listOfWorlds;
+
+    public final UtilsCollection utilsCollection = new UtilsCollection();
     public final GameState gamestate = new GameState(this);
     public final GameUtils gameutils = new GameUtils(this);
 
@@ -36,14 +44,36 @@ public final class HideAndSeek extends JavaPlugin {
     @Override
     public void onEnable() {
         instance = this;
+        listOfWorlds = Bukkit.getWorlds();
 
+        //create a copy of the main map
+        if(Bukkit.getWorld("temp_lobby") != null) {
+            utilsCollection.deleteWorld(Bukkit.getWorld("temp_lobby"));
+        }
+
+        utilsCollection.copyWorld(listOfWorlds.get(0), "temp_lobbymap");
+
+        //register Events
         pluginManager.registerEvents(new PlayerLoginListener(this), this);
         pluginManager.registerEvents(new PlayerJoinQuitListener(this), this);
         pluginManager.registerEvents(new GameProtectionListener(this), this);
 
+        //register all Events in ListernerCollection
         ListenerCollection listenercollection = new ListenerCollection(this);
         listenercollection.loadItemListeners();
 
         this.gamestate.setToLobbyState();
+    }
+
+    @Override
+    public void onDisable() {
+        //cleanup
+        if(Bukkit.getWorld("temp_lobby") != null) {
+            utilsCollection.deleteWorld(Bukkit.getWorld("temp_lobby"));
+        }
+
+        if(Bukkit.getWorld("temp_gamemap") != null) {
+            utilsCollection.deleteWorld(Bukkit.getWorld("temp_gamemap"));
+        }
     }
 }
